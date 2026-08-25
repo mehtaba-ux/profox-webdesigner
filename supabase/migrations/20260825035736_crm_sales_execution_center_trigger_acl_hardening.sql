1a3132735b0350fa5fd9f1b9aa5daed31a40a066
mehtaba-ux <mehtaba@profoxwebdesigner.com>
2026-08-25 09:33:42 +0530

    chore(db): mirror sales execution trigger ACL hardening

---
+++ supabase/migrations/20260825035736_crm_sales_execution_center_trigger_acl_hardening.sql
@@ -0,0 +1,2 @@
+revoke all on function public.crm_activity_plan_stop_from_event() from public, anon, authenticated, service_role;
+grant execute on function public.crm_activity_plan_stop_from_event() to postgres;
