d1babb3d359cdf56ab906e8411af3b60301b07ab
mehtaba-ux <mehtaba@profoxwebdesigner.com>
2026-08-28 17:11:09 +0530

    Add assignment pool to CRM lead command center

--- src/components/admin/CRMLeads.tsx
+++ src/components/admin/CRMLeads.tsx
@@ -1,1 +1,12 @@
-export { default } from './crm/CRMLeadWorkspace';
+import React from 'react';
+import CRMLeadWorkspace from './crm/CRMLeadWorkspace';
+import CRMLeadAssignmentQueue from './crm/CRMLeadAssignmentQueue';
+
+export default function CRMLeads(props: { onNavigate?: (tab: string) => void }) {
+  return (
+    <div className="space-y-6">
+      <CRMLeadAssignmentQueue />
+      <CRMLeadWorkspace {...props} />
+    </div>
+  );
+}
