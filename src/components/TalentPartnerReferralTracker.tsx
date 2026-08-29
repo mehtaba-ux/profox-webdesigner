6d9a7c8a718f9c7672cfabe8a64748b6d066296b
mehtaba-ux <mehtaba@profoxwebdesigner.com>
2026-08-29 14:58:49 +0530

    Add global Talent Partner referral tracker

---
+++ src/components/TalentPartnerReferralTracker.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { talentPartnerService } from '../lib/talentPartnerService';
+
+export default function TalentPartnerReferralTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!location.pathname.startsWith('/careers/')) return;
+    void talentPartnerService.captureReferralFromLocation().catch((error) => {
+      console.warn('Talent Partner referral tracking warning:', error);
+    });
+  }, [location.pathname, location.search]);
+
+  return null;
+}
