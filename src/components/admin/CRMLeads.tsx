d6da9ed7b2d2f8f7c2458354a9d240af56938143
mehtaba-ux <mehtaba@profoxwebdesigner.com>
2026-08-29 14:33:29 +0530

    Surface urgent assigned quote enquiries in CRM

--- src/components/admin/CRMLeads.tsx
+++ src/components/admin/CRMLeads.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CRMLeadWorkspace from './crm/CRMLeadWorkspace';
 import CRMLeadAssignmentQueue from './crm/CRMLeadAssignmentQueue';
+import CRMLeadFastResponseQueue from './crm/CRMLeadFastResponseQueue';
 
 export default function CRMLeads(props: { onNavigate?: (tab: string) => void }) {
   return (
     <div className="space-y-6">
       <CRMLeadAssignmentQueue />
+      <CRMLeadFastResponseQueue />
       <CRMLeadWorkspace {...props} />
     </div>
   );
